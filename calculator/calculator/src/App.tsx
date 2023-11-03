5ea014aa04bb3bea0ff7aa33456416a1d3eca839
Rafal <60304472+Bluefish5@users.noreply.github.com>
2023-11-03 20:30:30 +0100

    css + html changes

--- calculator/calculator/src/App.tsx
+++ calculator/calculator/src/App.tsx
@@ -6,27 +6,37 @@ function App() {
 
   return (
       <>
+        {/* <span className='mind'>one day or day one</span> */}
         <div className='main-calculator'>
           <div className='calculator-screen'>
             <p>0</p>
           </div>
           <div className='calculator-keyboard'>
+            <button>%</button>
+            <button>C</button>
+            <button>CE</button>
+            <button>&#60;</button>
+
             <button>1</button>
             <button>2</button>
             <button>3</button>
+            <button>+</button>
+
             <button>4</button>
             <button>5</button>
             <button>6</button>
+            <button>-</button>
+
             <button>7</button>
             <button>8</button>
             <button>9</button>
-            <button>9</button>
-            <button>+</button>
-            <button>-</button>
             <button>*</button>
-            <button>/</button>
+
+            <button>+/-</button>
+            <button>0</button>
+            <button>,</button>
+            <button>=</button>
           </div>
-          
         </div>
     </>
   )
