012a16fd4e4a4b29b4bb95af3b89b38945c65958
Rafal <60304472+Bluefish5@users.noreply.github.com>
2023-11-03 16:55:05 +0100

    Project init

---
+++ calculator/calculator/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+      <>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>9</button>
+          <button>+</button>
+          <button>-</button>
+          <button>×</button>
+          <button>÷</button>
+
+
+
+
+    </>
+  )
+}
+
+export default App
