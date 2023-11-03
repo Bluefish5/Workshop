66962394c598d8f2afd99285b4807630a5d58a4e
Rafal <60304472+Bluefish5@users.noreply.github.com>
2023-11-04 00:36:58 +0100

    background, better layout

--- calculator/calculator/src/App.tsx
+++ calculator/calculator/src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
+import img1 from './assets/ornament_1.jpg'
 import './App.css'
+
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -11,31 +13,44 @@ function App() {
           <div className='calculator-screen'>
             <p>0</p>
           </div>
-          <div className='calculator-keyboard'>
-            <button>%</button>
-            <button>C</button>
-            <button>CE</button>
-            <button>&#60;</button>
+          <div >
+            <div>
+              <button>%</button>
+              <button>C</button>
+              <button>CE</button>
+              <button>&#60;</button>
+            </div>
+            
+            <div>
+              <button>1</button>
+              <button>2</button>
+              <button>3</button>
+              <button>+</button>
+            </div>
+            
+            <div>
+              <button>4</button>
+              <button>5</button>
+              <button>6</button>
+              <button>-</button>
+            </div>
+            
 
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>+</button>
+            <div>
+              <button>7</button>
+              <button>8</button>
+              <button>9</button>
+              <button>*</button>
+            </div>
+            
 
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>-</button>
-
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>*</button>
-
-            <button>+/-</button>
-            <button>0</button>
-            <button>,</button>
-            <button>=</button>
+            <div>
+              <button>+/-</button>
+              <button>0</button>
+              <button>,</button>
+              <button>=</button>
+            </div>
+            
           </div>
         </div>
     </>
